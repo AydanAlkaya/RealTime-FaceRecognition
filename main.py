38fa24f8b8d659ad72366c8794ac9e50a9b6eae4
Aydan <56941797+AydanAlkaya@users.noreply.github.com>
2023-08-04 21:58:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,8 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import face_recognition
 import os
 import cv2
 import numpy as np
 import math
-
-
-# In[2]:
-
 
 def face_confidence(face_distance, face_match_threshold=0.6):
     range = (1.0 - face_match_threshold)
@@ -23,9 +13,6 @@ def face_confidence(face_distance, face_match_threshold=0.6):
     else:
         value = (linear_val + ((1.0 - linear_val) * math.pow((linear_val - 0.5) * 2, 0.2))) * 100
         return str(round(value, 2)) + '%'
-
-
-# In[ ]:
 
 
 class FaceRecognition:
